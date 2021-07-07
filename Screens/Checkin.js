a1e7bcc75d014a5b2a20b4f59742b0f349e3a6d2
programming 1 <Prog-1@yarub.com>
2021-07-07 14:07:53 +0300

    fixed data table

--- Screens/Checkin.js
+++ Screens/Checkin.js
@@ -6,24 +6,38 @@ import {DataTable} from "../Components/DataTable";
 import {Button, ButtonAdd, ButtonText, Colors, Container, Content, FlexEnd, FormArea} from "../Components/Styles";
 import RefreshHandler from "../Components/RefreshHandler";
 import {items} from "../DummyData";
-import {Feather,FontAwesome} from "@expo/vector-icons";
+import {Feather, FontAwesome} from "@expo/vector-icons";
 import Input from "../Components/Input";
+import {StyleSheet} from "react-native";
+import {Row, Rows, Table} from "react-native-table-component";
 
-const {white,red} = Colors
+const {white, red} = Colors
 
+const {secondary} = Colors;
 const Checkin = ({navigation}) => {
-    const tableHead = ['البند', 'الطول', 'حجم العبوة', 'التكلفة',''];
-    const [tableData ,setTableData] = useState( [
-        ['1', '2', '3', '4',<FontAwesome onPress={()=>removeRow(1)} name='times' color={red} style={{textAlign:'center'}}/>],
-        ['2', 'b', 'c', 'd',<FontAwesome onPress={()=>removeRow(2)} name='times' color={red} style={{textAlign:'center'}}/>],
-        ['3', '2', '3', '45789',<FontAwesome onPress={()=>removeRow(3)} name='times' color={red} style={{textAlign:'center'}}/>],
-        ['4', 'b', 'c', 'd',<FontAwesome onPress={()=>removeRow(4)} name='times' color={red} style={{textAlign:'center'}}/>]
+
+    const tableHead = ['البند', 'الطول', 'حجم العبوة', 'التكلفة', 'الاجراء'];
+
+    const [tableData, setTableData] = useState([
+        ['1', '2', '3', '4',
+            <FontAwesome onPress={() => removeRow(0)} name='times' color={red} style={{textAlign: 'center'}}/>],
+        ['2', 'b', 'c', 'd',
+            <FontAwesome onPress={() => removeRow(1)} name='times' color={red} style={{textAlign: 'center'}}/>],
+        ['3', '2', '3', '45789',
+            <FontAwesome onPress={() => removeRow(2)} name='times' color={red} style={{textAlign: 'center'}}/>],
+        ['4', 'b', 'c', 'd',
+            <FontAwesome onPress={() => removeRow(3)} name='times' color={red} style={{textAlign: 'center'}}/>]
     ]);
-    const removeRow=(id)=> {
-        setTableData(tableData.splice(id, 1));
-        console.log(id)
-    }
+
+    const removeRow = (id) => {
+        const newDataTableFields = tableData.filter((rows, i) => i !== id);
+        setTableData(newDataTableFields)
+    };
+
+
     return (
+
+
         <Layout>
             <Header title='الإدخال' navigation={navigation}/>
             <RefreshHandler>
@@ -38,7 +52,11 @@ const Checkin = ({navigation}) => {
                                     <ButtonText>إضافة</ButtonText>
                                 </ButtonAdd>
                             </FlexEnd>
-                            <DataTable tableHead={tableHead} tableData={tableData}   />
+                            <Table borderStyle={{borderWidth: 1, borderColor: '#eee', width: '100%'}}
+                                   style={{marginBottom: 15}}>
+                                <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
+                                <Rows data={tableData} textStyle={styles.text}/>
+                            </Table>
                             <Input
                                 label='الوقت المستغرق'
                                 icon='dashboard'
@@ -56,5 +74,10 @@ const Checkin = ({navigation}) => {
         </Layout>
     )
 }
+
+const styles = StyleSheet.create({
+    head: {height: 40, backgroundColor: secondary},
+    text: {margin: 6, textAlign: 'center'}
+});
 
 export default Checkin;
