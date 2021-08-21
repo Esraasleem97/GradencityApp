f8335c91bcf9c79e2b7f9a1c838c1d3dabc9a001
EsraaSaleem <you@example.com>
2021-08-21 10:09:42 +0300

    add upload img component

--- Components/Camera.js
+++ Components/Camera.js
@@ -11,27 +11,6 @@ function TakePicture() {
     // The path of the picked image
     const [pickedImagePath, setPickedImagePath] = useState('');
 
-    // This function is triggered when the "Select an image" button pressed
-    const showImagePicker = async () => {
-        // Ask the user for the permission to access the media library
-        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-        if (permissionResult.granted === false) {
-            alert("You've refused to allow this appp to access your photos!");
-            return;
-        }
-
-        const result = await ImagePicker.launchImageLibraryAsync();
-
-        // Explore the result
-        console.log(result);
-
-        if (!result.cancelled) {
-            setPickedImagePath(result.uri);
-            console.log(result.uri);
-        }
-    }
-
     // This function is triggered when the "Open camera" button pressed
     const openCamera = async () => {
         // Ask the user for the permission to access the camera
